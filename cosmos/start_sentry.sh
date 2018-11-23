804acd0435802f4df73279f2ebe900ce89089bec
trevorfu <bahuang2013@qq.com>
2018-11-23 19:17:15 +0800

    gaiad new version

--- cosmos/start_sentry.sh
+++ cosmos/start_sentry.sh
@@ -7,9 +7,7 @@ chmod +x *
 cd ~
 
 GAIAD_HOME=/data/.gaiad
-GAIACLI_HOME=/data/.gaiacli
 echo "当前指定的gaiad-home是: "$GAIAD_HOME
-echo "当前指定的gaiacli-home是: "$GAIACLI_HOME
 
 if [ ! -f $GAIAD_HOME"/config/genesis.json" ]; then
   echo "文件genesis.json不存在，执行'gaiad init'初始化："
@@ -20,7 +18,7 @@ if [ ! -f $GAIAD_HOME"/config/genesis.json" ]; then
   fi
 
   echo "当前初始化指定的chain-id是: "$CHAIN_ID
-  gaiad init --home=$GAIAD_HOME --home-client=$GAIACLI_HOME --chain-id=$CHAIN_ID --moniker=wancloud-sentry --name=wancloud
+  gaiad init --home=$GAIAD_HOME --chain-id=$CHAIN_ID --moniker=wancloud-sentry
 
   echo "请完成"$GAIAD_HOME"/config/目录下的genesis.json、config.toml配置更新，再执行本指令启动运行。"
   exit 0
