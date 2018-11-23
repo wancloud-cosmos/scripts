9ff05a1a8005287e6a5e01b3eefa26528e0d458b
trevorfu <bahuang2013@qq.com>
2018-11-23 14:35:42 +0800

    start scripts

--- cosmos/start_sentry.sh
+++ cosmos/start_sentry.sh
@@ -2,13 +2,6 @@
 
 CHAIN_ID=$1
 
-if [ ! -n "$CHAIN_ID" ]; then
-  echo "参数缺失，请指定：chain-id"
-  exit 1
-fi
-
-echo "当前指定的chain-id是: "$CHAIN_ID
-
 cd ~
 chmod +x gaia*
 
@@ -18,8 +11,15 @@ echo "当前指定的gaiad-home是: "$GAIAD_HOME
 echo "当前指定的gaiacli-home是: "$GAIACLI_HOME
 
 if [ ! -f $GAIAD_HOME"/config/genesis.json" ]; then
-  echo "genesis.json文件不存在，执行'gaiad init'初始化"
-  gaiad init --home=$GAIAD_HOME --home-client=$GAIACLI_HOME --chain-id=$CHAIN_ID --moniker=wancloud-sentry
+  echo "文件genesis.json不存在，执行'gaiad init'初始化："
+
+  if [ ! -n "$CHAIN_ID" ]; then
+    echo "参数缺失，请指定：chain-id"
+    exit 1
+  fi
+
+  echo "当前初始化指定的chain-id是: "$CHAIN_ID
+  gaiad init --home=$GAIAD_HOME --home-client=$GAIACLI_HOME --chain-id=$CHAIN_ID --moniker=wancloud-sentry --name=wancloud
 
   echo "请完成"$GAIAD_HOME"/config/目录下的genesis.json、config.toml配置更新，再执行本指令启动运行。"
   exit 0
