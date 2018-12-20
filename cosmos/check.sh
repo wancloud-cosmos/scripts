d4dda43ec3507f8c72a1894eb53865a91b99b0d7
trevorfu <bahuang2013@qq.com>
2018-12-20 18:33:08 +0800

    iris scripts

--- cosmos/check.sh
+++ cosmos/check.sh
@@ -5,7 +5,7 @@ source ~/.profile
 echo "Check gaiad process."
 
 FLAG=`ps -ef | grep "gaiad start" | grep -v grep | wc -l`
-if [ $FLAG -ne 0 ]; then
+if [ $FLAG -ne 1 ]; then
   nohup gaiad start --home /data/.gaiad > /data/gaiad.log 2>&1 &
   echo "Gaiad checked failure and restarted successfully."
 else
--- irishub/check.sh
+++ irishub/check.sh
@@ -4,8 +4,10 @@ source ~/.profile
 
 echo "Check IRIS process."
 
-FLAG=`ps -ef | grep "iris start" | wc -l`
-if [ $FLAG -ne 2 ]; then
-  nohup iris start --home /data/.gaiad > /data/iris.log 2>&1 &
+FLAG=`ps -ef | grep "iris start" | grep -v grep | wc -l`
+if [ $FLAG -ne 1 ]; then
+  nohup iris start --home /data/.iris > /data/iris.log 2>&1 &
   echo "IRIS process checked failure and restarted successfully."
+else
+  echo "IRIS process is running …… "
 fi
