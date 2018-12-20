23bcb2abe6a2785c97cdba3696c1e14bd5bbe5d1
trevorfu <bahuang2013@qq.com>
2018-12-20 18:27:03 +0800

    cosmos scripts

--- cosmos/check.sh
+++ cosmos/check.sh
@@ -4,8 +4,10 @@ source ~/.profile
 
 echo "Check gaiad process."
 
-FLAG=`ps -ef | grep "gaiad start" | wc -l`
-if [ $FLAG -ne 2 ]; then
+FLAG=`ps -ef | grep "gaiad start" | grep -v grep | wc -l`
+if [ $FLAG -ne 0 ]; then
   nohup gaiad start --home /data/.gaiad > /data/gaiad.log 2>&1 &
   echo "Gaiad checked failure and restarted successfully."
+else
+  echo "Gaiad is running …… "
 fi
