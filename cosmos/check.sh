20c019b3f06be1e254de6b0d1450e67900e50533
trevorfu <bahuang2013@qq.com>
2019-01-04 13:47:03 +0800

    set min-fee

--- cosmos/check.sh
+++ cosmos/check.sh
@@ -6,7 +6,7 @@ echo "Check gaiad process."
 
 FLAG=`ps -ef | grep "gaiad start" | grep -v grep | wc -l`
 if [ $FLAG -ne 1 ]; then
-  nohup gaiad start --home /data/.gaiad > /data/gaiad.log 2>&1 &
+  nohup gaiad start --home=/data/.gaiad --minimum_fees=100photinos > /data/gaiad.log 2>&1 &
   echo "Gaiad checked failure and restarted successfully."
 else
   echo "Gaiad is running …… "
