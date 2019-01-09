79ecfe1b09fbd32603b0576c4583168dc9a25359
trevorfu <bahuang2013@qq.com>
2019-01-09 19:00:12 +0800

    delegate

--- cosmos/auto_delegate.sh
+++ cosmos/auto_delegate.sh
@@ -1,20 +1,23 @@
- #!/bin/bash
+#!/bin/bash
 
 echo "Enter the password:"
-read -s password
+read -s PWD
+
+CHAIN_ID="game_of_stakes_3"
+ADDR_VALIDATOR="cosmosvaloper16p0uqvts5nvgy4rt5qeljg0e27kq5ay8lfmr8z"
+FROM="gos-new"
+FEE="120"
 
 while true
 do
-    echo "Delegating ..."
+    echo "Delegating($(date '+%Y-%m-%d %H:%M:%S')) ..."
 
-    denom=$(gaiacli query account cosmos16p0uqvts5nvgy4rt5qeljg0e27kq5ay86a0kt3 --chain-id=game_of_stakes_3 --trust-node=true | jq -r '.value.coins[0].denom')
-    amount=$(gaiacli query account cosmos16p0uqvts5nvgy4rt5qeljg0e27kq5ay86a0kt3 --chain-id=game_of_stakes_3 --trust-node=true | jq -r '.value.coins[0].amount')
+    DENOM=$(gaiacli query account cosmos16p0uqvts5nvgy4rt5qeljg0e27kq5ay86a0kt3 --chain-id=game_of_stakes_3 --trust-node=true | jq -r '.value.coins[0].denom')
+    AMOUNT=$(gaiacli query account cosmos16p0uqvts5nvgy4rt5qeljg0e27kq5ay86a0kt3 --chain-id=game_of_stakes_3 --trust-node=true | jq -r '.value.coins[0].amount')
 
-    if [ "$denom" == "STAKE" ]; then
-        echo "Current STAKE amount is: "$amount", now delegate them ..."
-        echo "gaiacli tx stake delegate --home /data/.gaiacli --validator=cosmosvaloper16p0uqvts5nvgy4rt5qeljg0e27kq5ay8lfmr8z --from=gos-new --chain-id=game_of_stakes_3 --fee=3020photinos --amount="${amount}"STAKE"
-
-        echo "$password" | gaiacli tx stake delegate --async --home /data/.gaiacli --validator=cosmosvaloper16p0uqvts5nvgy4rt5qeljg0e27kq5ay8lfmr8z --from=gos-new --chain-id=game_of_stakes_3 --fee=3020photinos --amount=${amount}STAKE
+    if [ "$DENOM" == "STAKE" ]; then
+        echo "Current STAKE amount is: "$AMOUNT", now delegate them ..."
+        echo "$PWD" | gaiacli tx stake delegate --async --home /data/.gaiacli --validator=${ADDR_VALIDATOR} --from=${FROM} --chain-id=${CHAIN_ID} --fee=${FEE}photinos --amount=${AMOUNT}STAKE
     fi
 
     sleep 600
